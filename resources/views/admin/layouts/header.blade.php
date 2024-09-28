dbb1aed2a22e7ed3dd26b30419b7cee45c5197b0
HaiDeng03 <haidangngo61@gmail.com>
2024-09-28 23:55:50 +0700

    dang

---
+++ resources/views/admin/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<!-- Header -->
+<div id="header">
+    <div id="toggle-btn">
+        <i class="fas fa-bars"></i>
+    </div>
+    <form class="search-form">
+        <input type="text" placeholder="Search..." />
+        <button type="submit"><i class="fas fa-search"></i></button>
+    </form>
+    <div class="user-info">
+        <span>Welcome, Admin</span>
+    </div>
+</div>
