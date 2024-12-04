bcf63e2a252bec82d0bb0c16fff682148c642502
Allexnguyen <nguyenndt68@gmail.com>
2024-12-04 23:04:33 +0700

    nguyen

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,7 @@ Route::group([
         // 'middleware' => ['role:role_3'],
     ], function () {
         route::controller(MusicController::class)->group(function () {
-            Route::match(['get', 'post'], '/list',  'list_music')->name('list-music')->middleware(['can:songs.index']);
+            Route::match(['get', 'post'], '/list',  'list_music')->name('list-music');
 
             Route::post('/search',  'search_song')->name('search-song');
             Route::get('/add',  'add_music')->name('add-music');
